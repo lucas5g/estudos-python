685c077899c1ed74b7a6779d04c10a27b13c1543
lucas.assuncao <lucas.assuncao@defensoria.mg.def.br>
2024-08-01 19:11:00 -0300

    rag

--- src/learning/hello.py
+++ src/learning/hello.py
@@ -1,65 +1,8 @@
-print(1 + 2)
-5 ** 2 
-10 / 4 
+LANGCHAIN_TRACING_V2=True
+# LANGCHAIN_ENDPOINT="https://api.smith.langchain.com"
+LANGCHAIN_API_KEY="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"
+LANGCHAIN_PROJECT="pr-scholarly-visitor-39"
+from langchain_openai import ChatOpenAI
 
-type(5)
-
-type(3.11)
-type(5.0)
-
-
-"Meu nome é lucas"
-type('Meu nome é lucas')
-
-type(int("5")) 
-
-5 + int("5")
-
-str(10)
-
-type(str(10))
-
-"50" + "10"
-
-"Olá " + "lucas"
-
-"Python" * 3
-
-"abc" - "c"
-
-len("Lucas")
-
-help(print)
-
-help(str)
-
-
-pi = 3.14
-raio = 5
-raio_ao_quadrado = raio ** 2
-
-pi * raio_ao_quadrado
-
-x = input("Digite um número")
-xNum = int(x)
-f"O valor do número digitado é {x}"
-
-xNum + 10
-
-
-name = input('Nome: ')
-age = int(input('Idada: '))
-
-f"Oi {name}"
-f"Seu tem {len(name)}d letras"
-f"Daqui 5 anos você terá {age + 5}"
-
-"_" * 10
-
-age = int(input('Digite sua idade: '))
-
-if age < 17:
-  "Você é menor de idade"
-else:
-  print('Vc tem maios de 18 anos')
-  
+llm = ChatOpenAI()
+llm.invoke("Hello, world!")
