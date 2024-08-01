72e5329208b2ce5a1588534834a27a691f467629
lucas.assuncao <lucas.assuncao@defensoria.mg.def.br>
2024-08-01 15:51:25 -0300

    project chat pdf

--- src/learning/asimov-chat-pdf/pages/Configuração.py
+++ src/learning/asimov-chat-pdf/pages/Configuração.py
@@ -1,25 +1,34 @@
-import json 
-import streamlit as st 
+import json
+
+import streamlit as st
+
 from configs import get_config
 from utils import PASTA_ARQUIVOS, cria_chain_conversa
-st.header("Página de configuração", divider=True)
 
-model_name = st.text_input("Modifique o modelo", value=get_config("model_name"))
-retrieval_search_type = st.text_input("Modifique o tipo de retrieval", value=get_config("retrieval_search_type"))
-retrieval_kwargs = st.text_input("Modifique os parâmetros de retrieval", value=json.dumps(get_config("retrieval_kwargs")))
-prompt = st.text_area("Modifique o prompt padrão", height=350, value=get_config("prompt"))
+def config_page():
+    st.header('Página de configuração', divider=True)
+
+    model_name = st.text_input('Modifique o modelo', value=get_config('model_name'))
+    retrieval_search_type = st.text_input('Modifique o tipo de retrieval', value=get_config('retrieval_search_type'))
+    retrieval_kwargs = st.text_input('Modifique os parâmetros de retrieval', value=json.dumps(get_config('retrieval_kwargs')))
+    prompt = st.text_area('Modifique o prompt padrão', height=350, value=get_config('prompt'))
 
 
-if st.button("Modifique parâmetros", use_container_width=True):
-    st.session_state["model_name"] = model_name
-    st.session_state["retrieval_search_type"] = retrieval_search_type
-    st.session_state["retrieval_kwargs"] = json.loads(retrieval_kwargs)
-    st.session_state["prompt"] = prompt
-    
-if st.button("Atualiza ChatBot", use_container_width=True):
-    if len(list(PASTA_ARQUIVOS.glob("*.pdf"))) == 0:
-        st.error("Você não adicionou nenhum arquivo PDF.")
-    else:
-        st.success("Inicializando o ChatBot...")
-        cria_chain_conversa()
+    if st.button('Salvar parâmetros', use_container_width=True):
+        retrieval_kwargs = json.loads(retrieval_kwargs.replace("'", '"'))
+        st.session_state['model_name'] = model_name
+        st.session_state['retrieval_search_type'] = retrieval_search_type
+        st.session_state['retrieval_kwargs'] = retrieval_kwargs
+        st.session_state['prompt'] = prompt
         st.rerun()
+    
+    if st.button('Atualizar ChatBot', use_container_width=True):
+        if len(list(PASTA_ARQUIVOS.glob('*.pdf'))) == 0:
+            st.error('Adicione arquivos .pdf para inicializar o chatbot')
+        else:
+            st.success('Inicializando o ChatBot...')
+            cria_chain_conversa()
+            st.rerun()
+
+
+config_page()
--- src/learning/asimov-chat-pdf/utils.py
+++ src/learning/asimov-chat-pdf/utils.py
@@ -1,19 +1,28 @@
-import streamlit as st 
-from langchain_openai import OpenAIEmbeddings, ChatOpenAI
-from langchain_community.document_loaders.pdf import PyPDFLoader
-from langchain_text_splitters import RecursiveCharacterTextSplitter
-from langchain.prompts import PromptTemplate
-from langchain_community.vectorstores import FAISS
-from langchain.memory import ConversationBufferMemory
-from langchain.chains import ConversationalRetrievalChain
 from pathlib import Path
+
+import streamlit as st
+from langchain.chains.conversational_retrieval.base import ConversationalRetrievalChain
+from langchain.memory import ConversationBufferMemory
+from langchain.prompts import PromptTemplate
+from langchain_community.document_loaders.pdf import PyPDFLoader
+from langchain_community.vectorstores.faiss import FAISS
+from langchain_text_splitters import RecursiveCharacterTextSplitter
+from langchain_openai.embeddings import OpenAIEmbeddings
+from langchain_openai.chat_models import ChatOpenAI
+
+from dotenv import load_dotenv, find_dotenv
+
 from configs import *
 
-PASTA_ARQUIVOS = Path(__file__).parent / "files"
+_ = load_dotenv(find_dotenv())
+
+
+PASTA_ARQUIVOS = Path(__file__).parent / 'arquivos'
+
 
 def importacao_documentos():
     documentos = []
-    for arquivo in PASTA_ARQUIVOS.glob("*.pdf"):
+    for arquivo in PASTA_ARQUIVOS.glob('*.pdf'):
         loader = PyPDFLoader(str(arquivo))
         documentos_arquivo = loader.load()
         documentos.extend(documentos_arquivo)
@@ -25,53 +34,45 @@ def split_de_documentos(documentos):
         chunk_overlap=250,
         separators=["/n\n", "\n", ".", " ", ""]
     )
-    
     documentos = recur_splitter.split_documents(documentos)
-    
+
     for i, doc in enumerate(documentos):
-        doc.metadata["source"] = doc.metadata["source"].split("/")[-1]
-        doc.metadata["doc_id"] = i
-    
+        doc.metadata['source'] = doc.metadata['source'].split('/')[-1]
+        doc.metadata['doc_id'] = i
     return documentos
 
 def cria_vector_store(documentos):
     embedding_model = OpenAIEmbeddings()
-    
     vector_store = FAISS.from_documents(
         documents=documentos,
         embedding=embedding_model
     )
-    
     return vector_store
 
 def cria_chain_conversa():
+
     documentos = importacao_documentos()
     documentos = split_de_documentos(documentos)
     vector_store = cria_vector_store(documentos)
-    
-    chat = ChatOpenAI(model="gpt-3.5-turbo")
+
+    chat = ChatOpenAI(model=get_config('model_name'))
     memory = ConversationBufferMemory(
         return_messages=True,
-        memory_key="chat_history", 
-        output_key="answer"
-    )
-    
+        memory_key='chat_history',
+        output_key='answer'
+        )
     retriever = vector_store.as_retriever(
-        search_type=get_config("retrieval_search_type"),
-        search_kwargs=get_config("retrieval_kwargs"),
-        filter_fn=lambda x: x.metadata["source"] == "documentos.pdf"
+        search_type=get_config('retrieval_search_type'),
+        search_kwargs=get_config('retrieval_kwargs')
     )
-    
-    prompt_template = PromptTemplate.from_template(get_config("retrieval_prompt_template"))
+    prompt = PromptTemplate.from_template(get_config('prompt'))
     chat_chain = ConversationalRetrievalChain.from_llm(
         llm=chat,
         memory=memory,
         retriever=retriever,
         return_source_documents=True,
         verbose=True,
-        combine_docs_chain_kwargs={"prompt": prompt_template}
+        combine_docs_chain_kwargs={'prompt': prompt}
     )
-    
-    st.session_state["chain"] = chat_chain
 
-
+    st.session_state['chain'] = chat_chain
