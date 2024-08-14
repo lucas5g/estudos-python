1708645e12de586475311c5be3e93ad9d78c53ed
lucas.assuncao <lucas.assuncao@defensoria.mg.def.br>
2024-08-14 15:04:48 -0300

    add template

--- src/django/chat-online/chat/views.py
+++ src/django/chat-online/chat/views.py
@@ -1,8 +1,9 @@
 from django.shortcuts import render
 from django.http import HttpResponse
-
+from .models import Message, Room
 
 def home(request):
-    return render(request, 'index.html', {
-        'variavel_nova': '123'
+    return render(request, 'chat/home.html', {
+        'rooms': Room.objects.all(),
+        'messages': Message.objects.all(),        
     })
