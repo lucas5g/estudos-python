cff8c344e1ee5683f8eab1a7e0210597db905cce
lucas.assuncao <lucas.assuncao@defensoria.mg.def.br>
2024-08-30 11:01:42 -0300

    add llama3.1

---
+++ src/pages/Chat_Llama-3.1.py
@@ -0,0 +1,31 @@
+import streamlit as st
+from langchain_groq import ChatGroq
+
+chat = ChatGroq(model="llama-3.1-8b-instant")
+
+#init
+if "messages" not in st.session_state:
+    st.session_state["messages"] = []
+
+
+##main
+
+st.header("Chat Llama3.1 Groq")
+
+for message in st.session_state["messages"]:
+    st.chat_message(message["role"]).markdown(message["content"])
+
+
+promtp = st.chat_input("Mande sua dúvida")
+
+if promtp:
+    st.chat_message('user').markdown(promtp)
+    st.session_state["messages"].append({"role": "user", "content": promtp})
+    
+    stream = chat.stream(st.session_state["messages"])
+    
+    res = st.chat_message("assistant").write_stream(stream)    
+    st.session_state["messages"].append({"role": "assistant", "content": res})
+    
+    
+    
